e58706afe468e181c42947ee6686646b40bbd032
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-07-23 10:35:39 -0500

    Problem 1 is solved, the function is able to return a list of words that contain 'th'

--- linqProblems/Program.cs
+++ linqProblems/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace linqProblems
+namespace linqProblemsProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Problem 1
+            var words = new List<string>() {"the", "bike", "this", "it", "thenth", "mathematics"};
+            var containsTh = words.Where(word => word.Contains("th")).Select(word => word);
+            foreach (var word in containsTh)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
